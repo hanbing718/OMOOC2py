3c1810c1e95ebc641d966ee1c0a927fa2d871501
hanbing718 <hanbing7181@163.com>
2015-10-24 23:02:50 +0800

    Update read.py

--- _src/om2py0w/0wex1/read.py
+++ _src/om2py0w/0wex1/read.py
@@ -1,2 +1,32 @@
-import dir.main
-print dir.main.name
+# -*- coding: utf-8 -*-
+import os
+ls =os.linesep
+
+#get filename
+
+fname = raw_input('input a file name to save filenames:%s' % ls)
+	
+
+#
+all=[]
+print"\nEnter lines *'.'by itself to quit).\n"
+
+#loop until user terminates input
+
+while True:
+    	entry=raw_input('>')
+	if entry=='EXIT':
+       		break
+    	elif entry=='READ':
+			fobj=open(fname,'r+')	
+			for eachline in fobj:
+				print eachline
+    	elif entry=='WRITE':
+       		all.append(entry)
+
+#
+fobj=open(fname,'w+')
+fobj.writelines(['%s%s' % (x,ls)for x in all])
+fobj.close()
+		
+	
